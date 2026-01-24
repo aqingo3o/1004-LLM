7fd834699837b6bf2d53f8f7d5f6b7d530ab2a37
Liv <universe5961@gmail.com>
2026-01-24 18:45:49 +0800

    add UI-like 的東西

--- interface/aqing/the-R-of-RAG.py
+++ interface/aqing/the-R-of-RAG.py
@@ -74,18 +74,28 @@ for c in chunks:
 ### ----------------------------  Make the Memory Bank --------------------------- ###
 memoryBank = torch.cat(sentenceEmbs, dim=0)
 
-### ---------------------------------  Try Query --------------------------------- ###
+### ----------------------------  cli is kind of ui... ---------------------------- ###
 #theQuery = 'what is the different between seyfert and qusar?'
-theQuery = 'is agn blackhole?'
-queryEmb = qingsEmbedder(theQuery, True)
-simiScores = queryEmb @ memoryBank.t()
 
-topScores, topIndex = torch.topk(simiScores, k=3)
-print(f'In all {len(chunks)} chunks, these are top three most simi:') # 他媽的英文亂講
-for i in range(len(topScores[0])):
-    print(f'Similarity scores: {(topScores[0][i]):.2f}')
-    print(f'--> {chunks[topIndex[0][i]]}')
-    print()
+print(f'The article you are looking at is {textName}.txt, ')
+print(f'and the using language mmodel is {LMName}.', end='\n\n')
+while True:
+    print('Enter your query here')
+    theQuery = input("(or 'quit' to exit) >>> ")
+    if theQuery=='quit':
+        print('byeeee ;))')
+        break
+    else:
+        time.sleep(2)
+        queryEmb = qingsEmbedder(theQuery, True)
+        simiScores = queryEmb @ memoryBank.t()
+        topScores, topIndex = torch.topk(simiScores, k=3)
+        print(f'In all {len(chunks)} chunks, these are top three most simi:') # 他媽的英文亂講
+        for i in range(len(topScores[0])):
+            print(f'Similarity scores: {(topScores[0][i]):.2f}')
+            print(f'--> {chunks[topIndex[0][i]]}')
+            print()
+        print('---------------------------------------------------------------------------------', end='\n\n')
 
 timeEnd = time.time()
 print(f'It took {(timeEnd-timeStart):.2f} seconds to finish the work.')
